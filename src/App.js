f297827ba2724e359b4feeecad4789b9780450b5
JhonVitorSS <jhonvitorsalles@gmail.com>
2022-07-27 12:01:46 -0300

    Semi-Finalizado, Aguardando os Filtros funcionais

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
+import React, { useState } from "react";
 import Effects from "./components/Effects";
+import PreviewImg from "./img/image-placeholder.svg"
 
 
 
 import "./Sass/main.sass"
 function App() {
+
+  const [imagem, setImagem] = useState(PreviewImg)
+  const [valorInput, setvalorInput]=useState(100)
+
+  const pegarImg = () => {
+    document.querySelector(".file-input").click()
+  }
+  const removerClassDisable = () => {
+    document.querySelector(".container").classList.remove("disable")
+
+  }
+  const mostrarImg = () => {
+    let arquivoEscolhido = document.querySelector(".file-input").files[0]
+    if (!arquivoEscolhido) return console.log("Você não escolheu a imagem");
+    setImagem(URL.createObjectURL(arquivoEscolhido))
+
+    removerClassDisable()
+  }
+
+
+  let filtrosBotoes = document.querySelectorAll(".filter button")
+  filtrosBotoes.forEach(botao => {
+    botao.addEventListener("click", () => {
+      document.querySelector(".filter .active").classList.remove("active")
+      botao.classList.add("active")
+
+      let filtroAtual = document.querySelector(".filter-info .name")
+      filtroAtual.innerHTML = botao.innerText
+
+
+    })
+  })
+
+
+
+
+
+
+
+
+
+
   return (
     <>
 
@@ -14,16 +58,16 @@ function App() {
           <div className="editor-panel">
             <div className="filter">
               <label className="title">Filters</label>
-              
+
               <div className="options">
-                <Effects/>
+                <Effects />
               </div>
               <div className="slider">
                 <div className="filter-info">
                   <p className="name">Brilho</p>
-                  <p className="value">100%</p>
+                  <p className="value">{valorInput}%</p>
                 </div>
-                <input type="range" value="100" min="0" max="200"/>
+                <input onChange={(e)=>setvalorInput(e.target.value)} type="range"  min="0" max="200" />
               </div>
             </div>
             <div className="rotate">
@@ -37,15 +81,15 @@ function App() {
             </div>
           </div>
           <div className="preview-img">
-            <img src="image-placeholder.svg" alt="preview-img"/>
+            <img src={imagem} alt="preview-img" className="imagemDaTela" />
           </div>
         </div>
         <div className="controls">
           <button class="reset-filter">Resetar Filtros</button>
           <div className="row">
-            <input type="file" className="file-input" accept="image/*" hidden/>
-              <button className="choose-img">Choose Image</button>
-            <button className="save-img">Save Image</button>
+            <input onChange={() => mostrarImg()} type="file" className="file-input" accept="image/*" hidden />
+            <button className="choose-img" onClick={() => pegarImg()}>Escolher Imagem</button>
+            <button className="save-img">Salvar Imagem</button>
           </div>
         </div>
       </div>
