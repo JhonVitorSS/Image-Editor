0ded2c122663f85828122e54a5df9f3a6154430c
JhonVitorSS <jhonvitorsalles@gmail.com>
2022-07-27 09:09:54 -0300

    Started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import Effects from "./components/Effects";
 
+
+
+import "./Sass/main.sass"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+
+      <div className="container disable">
+        <h2>Easy Image Editor</h2>
+        <div className="wrapper">
+          <div className="editor-panel">
+            <div className="filter">
+              <label className="title">Filters</label>
+              
+              <div className="options">
+                <Effects/>
+              </div>
+              <div className="slider">
+                <div className="filter-info">
+                  <p className="name">Brilho</p>
+                  <p className="value">100%</p>
+                </div>
+                <input type="range" value="100" min="0" max="200"/>
+              </div>
+            </div>
+            <div className="rotate">
+              <label className="title">Girar & Inverter</label>
+              <div className="options">
+                <button id="left"><i className="fa-solid fa-rotate-left"></i></button>
+                <button id="right"><i className="fa-solid fa-rotate-right"></i></button>
+                <button id="horizontal"><i className='bx bx-reflect-vertical'></i></button>
+                <button id="vertical"><i className='bx bx-reflect-horizontal' ></i></button>
+              </div>
+            </div>
+          </div>
+          <div className="preview-img">
+            <img src="image-placeholder.svg" alt="preview-img"/>
+          </div>
+        </div>
+        <div className="controls">
+          <button class="reset-filter">Resetar Filtros</button>
+          <div className="row">
+            <input type="file" className="file-input" accept="image/*" hidden/>
+              <button className="choose-img">Choose Image</button>
+            <button className="save-img">Save Image</button>
+          </div>
+        </div>
+      </div>
+
+
+
+
+    </>
   );
 }
 
---
+++ src/components/Effects.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Effects = () => {
+  return (
+    <div>
+      <button id="brightness" className="active">Brilho</button>
+      <button id="saturation">Saturação</button>
+      <button id="inversion">Negativo</button>
+      <button id="grayscale">Black & White</button>
+    </div>
+  );
+}
+
+export default Effects
